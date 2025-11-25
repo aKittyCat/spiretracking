d02b212a6cac7c3bdadb6c4803bfa6c9fdfe58c8
aKittyCat <holotaro@gmail.com>
2025-11-25 20:16:57 +0700

    .

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+// supabase-config.js
+const SUPABASE_URL = 'https://xtgtucdgqkgpnyyhmybe.supabase.co'; // เอาจาก Project Settings
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0Z3R1Y2RncWtncG55eWhteWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNjk2NDgsImV4cCI6MjA3OTY0NTY0OH0.jHhX7e2M_sU8Jch8XIGbG5_h6NNhftlMvl5Z45QjySE'; // Anon Key เอาจาก Project Settings
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
