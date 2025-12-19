eb39fdff0daa858a208c5655935b50a48604d60e
aKittyCat <holotaro@gmail.com>
2025-12-19 09:03:50 +0700

    .

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,11 @@
-const SUPABASE_URL = 'https://xtgtucdgqkgpnyyhmybe.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh0Z3R1Y2RncWtncG55eWhteWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwNjk2NDgsImV4cCI6MjA3OTY0NTY0OH0.jHhX7e2M_sU8Jch8XIGbG5_h6NNhftlMvl5Z45QjySE';
+const config = window.env || { SUPABASE_URL: '', SUPABASE_KEY: '' };
+
+const SUPABASE_URL = config.SUPABASE_URL;
+const SUPABASE_KEY = config.SUPABASE_KEY;
+
+if (!SUPABASE_URL || !SUPABASE_KEY) {
+    console.error('❌ ไม่พบ Supabase Configuration! กรุณาตรวจสอบไฟล์ env.js');
+}
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
