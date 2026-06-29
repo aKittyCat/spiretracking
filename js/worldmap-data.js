247e2607ed669db1ab78b90dac6b43cea94ab2c6
aKittyCat <holotaro@gmail.com>
2026-06-30 00:10:59 +0700

    a

---
+++ js/worldmap-data.js
@@ -0,0 +1,9 @@
+{
+    "id": 1782694564212,
+    "title": "test",
+    "image": "https://images.ctfassets.net/100cwma5ubtt/1GiiFUhJfnfFaV9apeWqYo/24d806d058e8892474c72197daec2486/FS_1440x810_cat-entertainment_7-reasons-you-should-adopt-a-cat.jpg",
+    "lore": "ทดสอบ 1234",
+    "iconType": "city",
+    "lat": 820,
+    "lng": 644
+},
