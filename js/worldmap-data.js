b130e936347995398f5a17713d6c6b4f4795646b
aKittyCat <holotaro@gmail.com>
2026-06-30 10:20:29 +0700

    map

--- js/worldmap-data.js
+++ js/worldmap-data.js
@@ -7,5 +7,14 @@ const loreData = [
         "iconType": "default",
         "lat": 1121.75,
         "lng": 462.25
+    },
+    {
+        "id": 1782789608829,
+        "title": "Waterdeep",
+        "image": "https://uploads.worldanvil.com/uploads/images/febed415ab6928dcea48309d732312c4.webp",
+        "lore": "หรือที่รู้จักกันในชื่อเมืองแห่งความรุ่งโรจน์ หรือมงกุฎแห่งทิศเหนือ",
+        "iconType": "city",
+        "lat": 1034.25,
+        "lng": 541.5
     }
 ];
