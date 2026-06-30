144b24267711380780b1607ef1792e19fa639f46
aKittyCat <holotaro@gmail.com>
2026-06-30 09:16:00 +0700

    map

--- js/worldmap-data.js
+++ js/worldmap-data.js
@@ -1,11 +1,11 @@
 const loreData = [
-//{
-//  "id": 1782694564212,
-//  "title": "test",
-//  "image": "https://images.ctfassets.net/100cwma5ubtt/1GiiFUhJfnfFaV9apeWqYo/24d806d058e8892474c72197daec2486/FS_1440x810_cat-entertainment_7-reasons-you-should-adopt-a-cat.jpg",
-//  "lore": "ทดสอบ 1234",
-//  "iconType": "city",
-//  "lat": 820,
-//  "lng": 644
-//}
+    {
+        "id": 1782785594830,
+        "title": "Neverwinter",
+        "image": "https://static.wikia.nocookie.net/forgottenrealms/images/8/80/NeverwinterHarbor.jpg/revision/latest/scale-to-width-down/1000?cb=20200825214415",
+        "lore": "",
+        "iconType": "default",
+        "lat": 1121.75,
+        "lng": 462.25
+    }
 ];
