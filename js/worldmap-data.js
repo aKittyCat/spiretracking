b332e8464632a7e689233adc9cf1311339c9ab7b
aKittyCat <holotaro@gmail.com>
2026-06-30 09:18:05 +0700

    map

--- js/worldmap-data.js
+++ js/worldmap-data.js
@@ -3,7 +3,7 @@ const loreData = [
         "id": 1782785594830,
         "title": "Neverwinter",
         "image": "https://static.wikia.nocookie.net/forgottenrealms/images/8/80/NeverwinterHarbor.jpg/revision/latest/scale-to-width-down/1000?cb=20200825214415",
-        "lore": "",
+        "lore": "หรือที่รู้จักกันในชื่อเมืองแห่งช่างฝีมือ และอัญมณีแห่งแดนเหนือ",
         "iconType": "default",
         "lat": 1121.75,
         "lng": 462.25
